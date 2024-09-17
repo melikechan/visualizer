05b34e16e43427c01390dcc08fef80f3f286ee88
Melike Vurucu <melikechan@tuta.io>
2024-09-17 13:08:58 +0300

    add: initials

---
+++ src/app/layout.tsx
@@ -0,0 +1,49 @@
+import { Inter } from "next/font/google";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { ThemeProvider } from "next-themes";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: "variable",
+  display: "swap",
+});
+
+export const metadata = {
+  title: "visualizer",
+  description: "visualize data structures and algorithms | by melikechan",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
+        <link rel="icon" href="/logo.svg" />
+      </head>
+      <body
+        className={
+          inter.className +
+          " bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-100"
+        }
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Navbar />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
